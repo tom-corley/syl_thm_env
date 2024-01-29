fda4ca4aa2aa92e5e34ae4585a93e8ba66adc56d
Roach2000 <roshan.kohli@hotmail.co.uk>
2024-01-29 17:21:11 +0000

    lol

--- lean_files_here/Sylow_Corollaries.lean
+++ lean_files_here/Sylow_Corollaries.lean
@@ -11,6 +11,8 @@ import Mathlib.GroupTheory.SpecificGroups.Cyclic
 import Mathlib.Data.Nat.Prime
 import Mathlib.GroupTheory.Sylow
 
+open scoped Classical
+
 variable (p : ℕ) [Fact p.Prime] (G : Type*) [Group G] [Fintype G]
 
 -- Cauchy's Theorem 1 - G contains an element of order p
@@ -18,9 +20,25 @@ theorem Cauchy_1 (hdvd : p ∣ Fintype.card G) : ∃ g : G, orderOf g = p := by
    exact exists_prime_orderOf_dvd_card p hdvd
    done
 
--- A group of order pq for primes p and q and such that p doesn't divide q-1, is the cyclic group of pq elements
-theorem C_pq (q : ℕ) [Fact q.Prime] (hdvd: p<q ∧ Fintype.card G = p*q) (h:¬(p ∣ q - 1)): IsCyclic G := by
-  have P := Sylow p G
-  have Q := Sylow q G
+theorem Cauchy_12 (hdvd : p ∣ Fintype.card G) : ∃ g : G, orderOf g = p := by
+   have P := Sylow p G
 
-  done
+   done
+
+-- A group of order pq for primes p and q and such that p doesn't divide q-1, is the cyclic group of pq elements
+theorem C_pq (q : ℕ) [hp : Fact p.Prime] [hq : Fact q.Prime] (hdvd: p<q ∧ Fintype.card G = p*q) (h:¬(p ∣ q - 1)): IsCyclic G := by
+   have P : Subgroup G := by exact Subgroup.center G
+   have Q : Subgroup G := by exact Subgroup.center G
+   have h0 : p ∣ Fintype.card G := by
+      rw [hdvd.2]
+      exact Nat.dvd_mul_right p q
+   have S1 P : Sylow p G := by exact P
+   have h1 : q ∣ Fintype.card G := by
+      rw [hdvd.2]
+      exact Nat.dvd_mul_left q p
+   have S2 Q : Sylow q G := by exact Q
+
+   have O :
+
+
+   done
