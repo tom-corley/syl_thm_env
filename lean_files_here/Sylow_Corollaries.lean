67f9641ef4d3f221e343a1577e2d351658ad4edb
Roach2000 <roshan.kohli@hotmail.co.uk>
2024-01-31 13:25:40 +0000

    l

--- lean_files_here/Sylow_Corollaries.lean
+++ lean_files_here/Sylow_Corollaries.lean
@@ -26,20 +26,21 @@ theorem Cauchy_12 (hdvd : p ∣ Fintype.card G) : ∃ g : G, orderOf g = p := by
    done
 
 -- A group of order pq for primes p and q and such that p doesn't divide q-1, is the cyclic group of pq elements
-theorem C_pq (q : ℕ) [hp : Fact (Nat.Prime p)] [hq : Fact q.Prime] (hdvd: p<q ∧ Fintype.card G = p*q) (h:¬(p ∣ q - 1)): IsCyclic G := by
+theorem C_pq (q : ℕ) [hp : Fact (Nat.Prime p)] [hq : Fact q.Prime] (hpq: p<q) (hpqq: Fintype.card G = p*q) (h:¬(p ∣ q - 1)): IsCyclic G := by
 --Define the Sylow p-subgroup
   have p0 : p ∣ Fintype.card G := by
-    rw [hdvd.2]
+    rw [hpqq]
     exact Nat.dvd_mul_right p q
   have p1 := Sylow.exists_subgroup_card_pow_prime p ((pow_one p).symm ▸ p0)
   rw [pow_one] at p1
   obtain ⟨P, hP⟩ : Sylow p G := by sorry
 --Define the Sylow q-subgroup
   have q0 : q ∣ Fintype.card G := by
-    rw [hdvd.2]
+    rw [hpqq]
     exact Nat.dvd_mul_left q p
   have q1 := Sylow.exists_subgroup_card_pow_prime q ((pow_one q).symm ▸ q0)
   rw [pow_one] at q1
   obtain ⟨Q, hQ⟩ : Sylow q G := by sorry
 --Show the Sylow p-subgroup is normal
+  have p2 : Nat.ModEq.dvd (card_sylow_modEq_one p G) 
   sorry
