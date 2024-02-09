5f775eeeb4f3c6073dc9a450c5e80c5f5be2bd6c
Antonina Sztyma <u2061677@live.warwick.ac.uk>
2024-02-09 17:23:53 +0000

    A

--- lean_files_here/t_file.lean
+++ lean_files_here/t_file.lean
@@ -1,20 +1,60 @@
 import Mathlib
 
 
--- (KB) for problem at the very end, it seems to be fixed by using `Finite` instead of `Fintype`.
-variable (p : ℕ) [Fact p.Prime] (G : Type*) [Group G] [Finite G]
+import Mathlib.Data.ZMod.Basic
+import Mathlib.GroupTheory.Index
+import Mathlib.Data.Finset.Card
+import Mathlib.GroupTheory.OrderOfElement
+import Mathlib.Data.Nat.Choose.Dvd
+import Mathlib.Data.Nat.Choose.Basic
+import Mathlib.Algebra.Group.Defs
+import Mathlib.GroupTheory.Subgroup.Basic
+import Mathlib.GroupTheory.SpecificGroups.Cyclic
+import Mathlib.Data.Nat.Prime
+import Mathlib.GroupTheory.Sylow
+import Mathlib.GroupTheory.Coset
 
--- (KB) Your comment below should be a *docstring*, so start with `/--`, finish with `-/`
+open scoped Classical
+
+variable (p : ℕ) [Fact p.Prime] (G : Type*) [Group G] [Fintype G]
+
+-- Cauchy's Theorem 1 - G contains an element of order p
+theorem Cauchy_1 (hdvd : p ∣ Fintype.card G) : ∃ g : G, orderOf g = p := by
+   exact exists_prime_orderOf_dvd_card p hdvd
+   done
+
+theorem Cauchy_12 (hdvd : p ∣ Fintype.card G) : ∃ g : G, orderOf g = p := by
+   have P := Sylow p G
+
+   done
+
 -- A group of order pq for primes p and q and such that p doesn't divide q-1, is the cyclic group of pq elements
-theorem C_pq (q : ℕ) [hp : Fact p.Prime] [hq : Fact q.Prime] (hdvd: p<q ∧ Nat.card G = p*q) (h:¬(p ∣ q - 1)): IsCyclic G := by
-  -- (KB) let's take a look at the statement of our theorem. We seem to have the `Fact` that p is prime
-  -- twice! That might confuse the typeclass system. Look at your code. You wrote it twice,
-  -- so now we have it twice.
-  -- Also, why is `hdvd` two hypotheses? It could be two, `hptlq` and `hpq`.
-  have h0 : p ∣ Nat.card G := by
-    rw [hdvd.2]
-    exact Nat.dvd_mul_right p q -- (KB) I hope you found this with `exact?`. The goal is
-    -- *obviously* going to be in the library, it's such a natural statement.
-  have h1 : ∃ P : Sylow p G, Nat.card P = p := by
-    sorry
+theorem C_pq (q : ℕ) [hp : Fact (Nat.Prime p)] [hq : Fact q.Prime] (hpq: p<q) (hpqq: Fintype.card G = p*q) (h:¬(p ∣ q - 1)): IsCyclic G := by
+--Define the Sylow p-subgroup
+  have p0 : p ∣ Fintype.card G := by
+    rw [hpqq]
+    exact Nat.dvd_mul_right p q
+  have p1 := Sylow.exists_subgroup_card_pow_prime p ((pow_one p).symm ▸ p0)
+  rw [pow_one] at p1
+  obtain ⟨P, hP⟩ := by exact p1
+--Define the Sylow q-subgroup
+  have q0 : q ∣ Fintype.card G := by
+    rw [hpqq]
+    exact Nat.dvd_mul_left q p
+  have q1 := Sylow.exists_subgroup_card_pow_prime q ((pow_one q).symm ▸ q0)
+  rw [pow_one] at q1
+  obtain ⟨Q, hQ⟩ := by exact q1
+--Show the Sylow p-subgroup is normal
+  have p2 : Fintype.card (Sylow p G) = 1 := by
+    apply?
+
+
+
+
+
+
+
+
+
+
   sorry
