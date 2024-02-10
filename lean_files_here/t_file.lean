2b4467cd28388fb6e3d1ea8dffef9c4d6bf938f0
Antonina Sztyma <u2061677@live.warwick.ac.uk>
2024-02-10 19:04:20 +0000

    A

--- lean_files_here/t_file.lean
+++ lean_files_here/t_file.lean
@@ -1,4 +1,9 @@
 import Mathlib.GroupTheory.Sylow
+import Mathlib.Data.Finset.Card
+import Mathlib.Data.ZMod.Basic -- Contains definition of modular equality
+import Mathlib.GroupTheory.Index -- Contains definition of group index
+import Mathlib.GroupTheory.Subgroup.Basic -- Contains definition of a normal subgroup
+import Mathlib.Data.Nat.Prime -- Contains definition of a prime
 
 -- Assume p is a prime and G is a finite group
 variable (p : ℕ) [Fact p.Prime] (G : Type*) [Group G] [Fintype G]
@@ -12,48 +17,35 @@ example (hG : Fintype.card G = 20) : ¬ IsSimpleGroup G := by
   have h₁ : Fintype.card G = 2^2 * 5 := by linarith [hG]
 
   -- Prove that 5 divides the order of the group G
-  have h₂ : 5 ∣ Fintype.card G := by use 1 -- 5 divides 20 (20 = 4 * 5)
+  have h₂ : 5 ∣ Fintype.card G := by use 4 -- 5 divides 20 (20 = 4 * 5)
 
-  -- According to Sylow's theorem, there exists at least one Sylow 5-subgroup
-  have h₃ : Sylow.exists' G 5 := Sylow.exists_prime_pow h₀
+  -- Apply Sylow's theorem to conclude the existence of a Sylow 5-subgroup
+  -- The theorem guarantees a subgroup of order 5^1, as 5 is a prime dividing the group's order
+  have h₃ := Sylow.exists_subgroup_card_pow_prime 5 ((pow_one 5).symm ▸ h₀)  -- I don't know how to fix this
 
-  -- Use Sylow's theorem which states that the number of Sylow p-subgroups is congruent to 1 mod p
-  -- and it divides the order of the group
-  have h₄ : ∃ n : ℕ, n ≡ 1 [MOD 5] ∧ n ∣ Fintype.card G ∧ IsSylow G 5 n := Sylow.exists_of_card h₀ h₂
+  -- We extract the actual subgroup Q which satisfies the Sylow p-subgroup conditions for p=5
+  obtain ⟨Q, hQ⟩ := by exact h₃
 
-  -- There must be a natural number n that satisfies the conditions of being a Sylow p-subgroup
-  obtain ⟨n, hn₁, hn₂, hnP⟩ := h₄
+  -- Now we use Sylow's theorems to analyse the number of such subgroups
 
-  -- There are only two possibilities for n, 1 or 4, as these are the only divisors of 20
-  -- that are also congruent to 1 mod 5
-  have h₅ : n = 4 ∨ n = 1 := by
-    have : n ∣ 20 := by linarith [Fintype.card G, hn₂]
-    have : n > 0 := Nat.pos_of_ne_zero (λ h, by linarith [h, hn₁])
-    interval_cases n; linarith -- interval_cases will test the possible values for n
+   -- Use Sylow's theorems to deduce the number of Sylow p-subgroups is congruent to 1 mod p
+  have h₄ : Fintype.card (Sylow 5 G) ≡ 1 [MOD 5] := by sorry
+  have h₅ : Fintype.card (Sylow 5 G) = 5 ∨ Fintype.card (Sylow 5 G) = 1 := by sorry
 
-  -- We argue by contradiction that if n > 1, G cannot be simple
-  have h₆ : ¬(n > 1 ∧ IsSimpleGroup G) := by
-    rintro ⟨hn_gt_one, hG_simple⟩
-    obtain ⟨P, hP⟩ := Sylow.exists_prime_pow h₀
-    -- If there are multiple Sylow 5-subgroups, they cannot be the same, contradicting the simplicity of G
-    obtain ⟨Q, hQ, hP_ne_Q⟩ : ∃ Q, IsSylow G 5 Q ∧ P ≠ Q := hG_simple.exists_of_card_sylow h₀ hnP hn_gt_one
-    -- If P is not a subset of Q, then they must intersect trivially
-    have hPQ : ¬(P ⊆ Q) := λ h, hP_ne_Q (Subgroup.eq_of_le_of_card_eq h hP.card_sylow hQ.card_sylow)
-    -- Therefore, there must be a normal subgroup, which contradicts the assumption that G is simple
-    have hGp : ∃ p, p < Fintype.card G ∧ IsNormal G p := by
-      obtain ⟨p, hp, hP⟩ := Sylow.normal_of_conjugate h₀ hP hQ hPQ
-      exact ⟨p, hp, hP⟩
-    exact hG_simple.not_exists_normal hGp
+  -- Show that the number of Sylow 5-subgroups divides the order of the group divided by the order of a Sylow 5-subgroup
+  have h₆ : (Fintype.card (Sylow 5 G)) ∣ (Fintype.card G)/(Fintype.card Q) := by sorry
+  have h₇ : Fintype.card (Sylow 5 G) = 1 ∨ Fintype.card (Sylow 5 G) = 4 := by sorry
 
-  -- Since there must be exactly one Sylow 5-subgroup, we conclude n = 1
-  have h₇ : n = 1 := by cases h₅; contradiction
+  -- Establish that there is exactly one Sylow 5-subgroup in G
+  have h₈ : Fintype.card (Sylow 5 G) = 1 := by sorry
 
-  -- As a result, there is a unique Sylow 5-subgroup, which implies it must be normal
-  have h₈ : ∃! P, IsSylow G 5 P := hnP.unique_sylow h₀ h₇
+  -- Conclude the existence and uniqueness of the Sylow 5-subgroup, implying it is normal
+  -- The uniqueness of the Sylow subgroup follows from h₈ and the properties of Sylow subgroups
+  have h₉ : ∃! P, IsSylow G 5 P := by sorry
 
-  -- We can now conclude that G is not simple because it has a normal subgroup of order 5
-  show ¬ IsSimpleGroup G := by
-    rintro hG_simple
-    obtain ⟨P, hP_unique⟩ := h₈
-    -- If there is a unique Sylow 5-subgroup, it is necessarily normal
-    have hP_normal : IsNormal
+  -- Prove that the unique Sylow subgroup P is a normal subgroup of G
+  -- This will use the fact that a unique Sylow subgroup is always normal
+  have h₁₀ : Subgroup.Normal P := by sorry
+
+  -- Conclude that G is not simple because it has a normal subgroup of order 5
+  -- This final step uses the definition of a simple group, which cannot have non-trivial normal subgroups
