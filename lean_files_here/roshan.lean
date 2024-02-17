ddab65005e9d66c53258da617991cc4ce7bef2ac
Antonina Sztyma <u2061677@live.warwick.ac.uk>
2024-02-17 21:25:08 +0000

    A

--- lean_files_here/roshan.lean
+++ lean_files_here/roshan.lean
@@ -1,10 +1,17 @@
 import Mathlib.Data.Nat.Prime
 import Mathlib.GroupTheory.Sylow
+import Mathlib.GroupTheory.Subgroup.Simple
 
 open scoped Classical
 
 variable (p : ℕ) [Fact p.Prime] (q : ℕ) [Fact q.Prime] (G : Type*) [Group G] [Fintype G]
 
-theorem intersection_trivial (P : Sylow p G) (Q : Sylow q G) (hG: Fintype.card G = pq) (hp: Fintype.card P = p)
-(hq: Fintype.card Q = q) (hp1: P.Normal) (hq1: Q.Normal) : (P : Subgroup G) ⊓ (Q : Subgroup G) = ⊥ := by
-  sorry
+example (hG : Fintype.card G = 20) (Q: Subgroup G) (h: Q.Normal): ¬ IsSimpleGroup G := by
+  have h1 : Q ≠ ⊥ := by sorry
+
+  have h2 : Q ≠ ⊤ := by sorry
+
+  intro h3
+  have := h3.eq_bot_or_eq_top_of_normal Q h
+  cases this <;> contradiction -- `exact this.elim h1 h2` will also work here
+  done
