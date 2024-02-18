c8843510ae79b8de74d646c7a9a0292516818b45
Roach2000 <roshan.kohli@hotmail.co.uk>
2024-02-18 00:57:28 +0000

    .

--- lean_files_here/roshan.lean
+++ lean_files_here/roshan.lean
@@ -17,5 +17,4 @@ variable (p : ℕ) [Fact p.Prime] (q : ℕ) [Fact q.Prime] (G : Type*) [Group G]
 variable (G : Type*) [Group G] [Fintype G] (P Q : Subgroup G)
 
 theorem mul (g : P) (k : Q) (h : (P : Subgroup G) ⊓ (Q : Subgroup G) = ⊥) (hP : (P : Subgroup G).Normal)
-(hQ : (Q : Subgroup G).Normal) : (g*k*g⁻¹*k⁻¹ : G) ∈ P := by
-  exact?
+(hQ : (Q : Subgroup G).Normal) : (g*k*g⁻¹*k⁻¹ : G) ∈ P := by sorry
