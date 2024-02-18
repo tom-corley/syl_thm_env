d78826418b617a57ae59d6d4fa511defa4faf923
Roach2000 <roshan.kohli@hotmail.co.uk>
2024-02-18 00:14:37 +0000

    .

--- lean_files_here/Sylow_Corollaries.lean
+++ lean_files_here/Sylow_Corollaries.lean
@@ -51,7 +51,12 @@ theorem unique_of_normal [Finite (Sylow p G)] (P : Sylow p G)
 -- If G has a unique Sylow p-subgroup P, then it is normal in G
 theorem normal_of_unique [Finite (Sylow p G)] (P : Sylow p G)
 (h : Fintype.card (Sylow p G) = 1) : (P : Subgroup G).Normal := by
-  apply?
+  have h1 : Fintype.card (Sylow p G) = Subgroup.index (Subgroup.normalizer (P : Subgroup G)) := by
+    exact card_sylow_eq_index_normalizer P
+  rw [h] at h1
+  have h2 : Subgroup.normalizer (P : Subgroup G) = ⊤ := by exact Subgroup.index_eq_one.mp (id h1.symm)
+  exact Subgroup.normalizer_eq_top.mp h2
+
 
 --==================================================================================
 --              GROUPS OF ORDER pq ARE CYCLIC (GIVEN p DOESN'T DIVIDE q)
