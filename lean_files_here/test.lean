249749091dff6b1630169b45e319248f0c73b66d
Tom <tomcorley86@gmail.com>
2024-02-18 23:09:22 +0000

    tidying repo

--- lean_files_here/test.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib.GroupTheory.Sylow
-
-open scoped Classical
-
-variable (p : ℕ) [Fact p.Prime] (q : ℕ) [Fact q.Prime] (G : Type*) [Group G] [Fintype G]
-
-theorem must_divide_index (Q : Sylow 11 G) (h1: Fintype.card G = 462) (h2: Fintype.card (Q : Subgroup G) = 11) (h3: Fintype.card (Sylow 11 G) ≡ 1 [MOD 11]) (h4: (Fintype.card (Sylow 11 G)) ∣ (Q : Subgroup G).index ) (h5: (Q : Subgroup G).index = 42) : Fintype.card (Sylow 11 G) = 1 ∨ Fintype.card (Sylow 11 G) = 42 ∨ Fintype.card (Sylow 11 G) = 21 ∨ Fintype.card (Sylow 11 G) = 2 ∨ Fintype.card (Sylow 11 G) = 3 ∨ Fintype.card (Sylow 11 G) = 14 := by
-  sorry
